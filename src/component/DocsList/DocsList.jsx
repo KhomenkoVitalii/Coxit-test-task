0fc8cf1191d0afe5f5e9da7bffee77092b672d0d
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-03-07 01:10:53 +0200

    style DocsList: added name of columns for desktop

--- src/component/DocsList/DocsList.jsx
+++ src/component/DocsList/DocsList.jsx
@@ -4,7 +4,14 @@ import './DocsList.scss';
 const DocsList = ({ data }) => {
     return (
         <div className="documents">
-            <div className="documents_header"></div>
+            <div className="document columns">
+                <span className="document_column_name">Name</span>
+                <span className="document_column"></span>
+                <span className="document_column_status">Status</span>
+                <span className="document_column_date">Date</span>
+                <span className="document_column_size">Size</span>
+                <span className="document_column_comment">Comment</span>
+            </div>
             {data && data.records && data.records.map((item, key) => (
                 <Document key={key} data={item} />
             ))}
